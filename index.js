461c9bae4872ebd94806ab25e8dceead8f2d4341
bergi <bergi@axolotlfarm.org>
2018-03-11 00:49:44 +0100

    export ToArray and gccPhat in main module

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ module.exports = {
   AutoSpeechToText: require('./lib/AutoSpeechToText'),
   ChannelPicker: require('./lib/ChannelPicker'),
   Filter: require('./lib/Filter'),
+  gccPhat: require('./lib/gccPhat'),
   GoogleSpeechToText: require('./lib/GoogleSpeechToText'),
   Recorder: require('./lib/Recorder'),
   Slice: require('./lib/Slice'),
   SnowboyKeywordSpotting: require('./lib/SnowboyKeywordSpotting'),
+  ToArray: require('./lib/ToArray'),
   WitSpeechToText: require('./lib/WitSpeechToText')
 }
