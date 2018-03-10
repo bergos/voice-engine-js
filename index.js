a7ee41292c8e06cade77487d8d477ad8703a37f9
bergi <bergi@axolotlfarm.org>
2018-03-11 00:43:03 +0100

    moved ReSpeaker specific code to separate package

--- index.js
+++ index.js
@@ -1,23 +1,11 @@
-const AutoKeywordSpotting = require('./lib/AutoKeywordSpotting')
-const AutoSpeechToText = require('./lib/AutoSpeechToText')
-const ChannelPicker = require('./lib/ChannelPicker')
-const Filter = require('./lib/Filter')
-const GoogleSpeechToText = require('./lib/GoogleSpeechToText')
-const Recorder = require('./lib/Recorder')
-const ReSpeaker4MicPixelRing = require('./lib/ReSpeaker4MicPixelRing')
-const Slice = require('./lib/Slice')
-const SnowboyKeywordSpotting = require('./lib/SnowboyKeywordSpotting')
-const WitSpeechToText = require('./lib/WitSpeechToText')
-
 module.exports = {
-  AutoKeywordSpotting,
-  AutoSpeechToText,
-  ChannelPicker,
-  Filter,
-  GoogleSpeechToText,
-  Recorder,
-  ReSpeaker4MicPixelRing,
-  Slice,
-  SnowboyKeywordSpotting,
-  WitSpeechToText
+  AutoKeywordSpotting: require('./lib/AutoKeywordSpotting'),
+  AutoSpeechToText: require('./lib/AutoSpeechToText'),
+  ChannelPicker: require('./lib/ChannelPicker'),
+  Filter: require('./lib/Filter'),
+  GoogleSpeechToText: require('./lib/GoogleSpeechToText'),
+  Recorder: require('./lib/Recorder'),
+  Slice: require('./lib/Slice'),
+  SnowboyKeywordSpotting: require('./lib/SnowboyKeywordSpotting'),
+  WitSpeechToText: require('./lib/WitSpeechToText')
 }
